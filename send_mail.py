fe728ef00c1f2247b66a820432d5bb59a8dffc48
dingyn-Reno <57473947+dingyn-Reno@users.noreply.github.com>
2022-11-13 16:51:58 +0800

    Update send_mail.py

--- send_mail.py
+++ send_mail.py
@@ -3,16 +3,16 @@ from email.mime.text import MIMEText
 
 def send(text):
     # 设置服务器所需信息
-    # 163邮箱服务器地址
-    mail_host = 'smtp.exmail.qq.com'
-    # 163用户名
-    mail_user = 'dingyn@djinni.club'
+    # 邮箱服务器地址
+    mail_host = ''
+    # 用户名
+    mail_user = ''
     # 密码(部分邮箱为授权码)
-    mail_pass = 'Ding1234'
+    mail_pass = ''
     # 邮件发送方邮箱地址
-    sender = 'dingyn@djinni.club'
+    sender = ''
     # 邮件接受方邮箱地址，注意需要[]包裹，这意味着你可以写多个邮件地址群发
-    receivers = ['dingyn@djinni.club']
+    receivers = ['']
 
     # 设置email信息
     # 邮件内容设置
